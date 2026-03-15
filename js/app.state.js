25dc54d25f8bc6d98075ade84c0ca1f38861885b
Gaox-rgb <geniosdeltalento@gmail.com>
2026-03-14 18:41:39 -0600

    ORDEN DE EJECUCIÓN: Aniquilación de estructura fragmentada. Monolito de combate implantado.

--- js/app.state.js
+++
@@ -1,7 +0,0 @@
-// js/app.state.js
-console.log("MAKUMOTO® DEBUG: app.state.js comenzando. (Asume window.app ya inicializado por app.js).");
-// NO inicializar window.app aquí. Se inicializa en app.js
-window.app.state = {
-    // El estado isModalShown ha sido expurgado.
-};
-console.log("MAKUMOTO® DEBUG: app.state.js finalizado.");
