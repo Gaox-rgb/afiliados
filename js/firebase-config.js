e38ce8489561ae9179c6c362c8869ef57795feb0
Gaox-rgb <geniosdeltalento@gmail.com>
2026-03-19 23:43:12 -0600

    Resolve security alert and add .gitignore

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+// ESTE ARCHIVO NUNCA DEBE SER SUBIDO A GITHUB.
+// Contiene las claves de configuración de Firebase.
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkKg8saxM2un-qXYTwN7qpO0w-2yTpkvk", // Inserta la nueva clave revocando la anterior
+  authDomain: "makumoto-app-2026.firebaseapp.com",
+  projectId: "makumoto-app-2026",
+  storageBucket: "makumoto-app-2026.appspot.com",
+  messagingSenderId: "398366272130",
+  appId: "1:398366272130:web:073b05e75ed299534ce29b"
+};
